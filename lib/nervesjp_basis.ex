203b8c1432467144bb78e421858f095a7ac1b6bb
takasehideki <takase@i.kyoto-u.ac.jp>
2020-11-25 09:01:54 +0000

    mix nerves.new --target rpi4 nervesjp_basis

---
+++ lib/nervesjp_basis.ex
@@ -0,0 +1,18 @@
+defmodule NervesjpBasis do
+  @moduledoc """
+  Documentation for NervesjpBasis.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesjpBasis.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nervesjp_basis_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesjpBasisTest do
+  use ExUnit.Case
+  doctest NervesjpBasis
+
+  test "greets the world" do
+    assert NervesjpBasis.hello() == :world
+  end
+end
